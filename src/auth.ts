cfa8448ecdc6de6359f6b9000c0bdbf628b1dfa3
Hamdani Ilham <d41113512@gmail.com>
2024-07-25 07:43:56 +0800

    google login

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,14 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { db } from "./db/drizzle";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
    adapter: DrizzleAdapter(db),
-   providers: [Github],
+   providers: [Github, Google],
    pages: {
       signIn: "/login",
-   }
+      error: "/login",
+   },
 });
