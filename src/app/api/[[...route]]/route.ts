dad908901ebbe40734a98a7f24d0d2ba0e1bef3a
Hamdani Ilham <d41113512@gmail.com>
2024-07-25 14:38:03 +0800

    authjs and hono, protected all route except user route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,15 +1,36 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { handle } from "hono/vercel";
 import user from "./user";
 import images from "./images";
+import {
+  type AuthConfig,
+  initAuthConfig,
+  verifyAuth,
+  authHandler,
+} from "@hono/auth-js";
+import authConfig from "@/auth.config";
 
 export const runtime = "nodejs";
 
 const app = new Hono().basePath("/api");
 
-const route = app
-    .route("/user", user)
-    .route("/images", images)
+function getAuthConfig(c: Context): AuthConfig {
+  return {
+    secret: c.env.AUTH_SECRET,
+    ...authConfig,
+  };
+}
+app.use("*", initAuthConfig(getAuthConfig));
+
+app.use("*", async (c, next) => {
+  if (c.req.path.startsWith("/api/user")) {
+    return next();
+  } else {
+    await verifyAuth()(c, next);
+  }
+});
+
+const route = app.route("/user", user).route("/images", images);
 
 export const GET = handle(app);
 export const POST = handle(app);
--- src/app/api/[[...route]]/user.ts
+++ src/app/api/[[...route]]/user.ts
@@ -9,27 +9,32 @@ import { db } from "@/db/drizzle";
 import { users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
-const app = new Hono().post(
-  "/register",
-  zValidator("json", z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(8).max(32),
-  })),
-  async (c) => {
-    let data = c.req.valid("json");
-    data.password = await bcrypt.hash(data.password, 10);
+const app = new Hono()
+  .post(
+    "/register",
+    zValidator(
+      "json",
+      z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(8).max(32),
+      })
+    ),
+    async (c) => {
+      let data = c.req.valid("json");
+      data.password = await bcrypt.hash(data.password, 10);
 
-    const [user] = await db.select()
-                        .from(users)
-                        .where(eq(users.email, data.email));
-    if (user) {
-      return c.json({ errors: "User already exists" }, 400);
+      const [user] = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, data.email));
+      if (user) {
+        return c.json({ errors: "User already exists" }, 400);
+      }
+
+      await db.insert(users).values(data);
+      return c.json(null, 200);
     }
-
-    await db.insert(users).values(data);
-    return c.json(null, 200);
-  }
-);
+  );
 
 export default app;
